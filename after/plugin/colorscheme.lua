6c4a1cb4d4eaad83dd6cb64f08bddae2298c13e2
khuong <dinhquangkhuong5566@gmail.com>
2024-01-19 23:17:26 +0700

    new

---
+++ after/plugin/colorscheme.lua
@@ -0,0 +1,12 @@
+function Color()
+  vim.api.nvim_set_hl_ns(1)
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    Color()
+  end
+})
+
