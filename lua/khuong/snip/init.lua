652967f3fe3ff280eab51671a9746ad552c0d573
khuong <dinhquangkhuong5566@gmail.com>
2024-03-09 21:22:52 +0700

    remove init file

--- lua/khuong/snip/init.lua
+++ lua/khuong/snip/init.lua
@@ -3,19 +3,24 @@ local cwdContent = vim.split(vim.fn.glob(cwDir .. "/*"), '\n', { trimempty = tru
 
 local snipets = {}
 
-for _key, value in pairs(cwdContent) do
+for _, value in pairs(cwdContent) do
 	local file = vim.split(value, '/', { trimempty = true })
 	local filetype = vim.split(file[#file], '.', { plain = true })
+
+	if filetype[1] == "init" then
+		goto continue
+	end
 
 	table.insert(snipets, {
 		filetype = filetype[1],
 		used = false,
 	})
+	::continue::
 end
 
 vim.api.nvim_create_autocmd("BufEnter", {
 	callback = function()
-		for _key, value in pairs(snipets) do
+		for _, value in pairs(snipets) do
 			if vim.bo.filetype == value.filetype and not value.used then
 				value.used = true
 				require("khuong.snip." .. vim.bo.filetype)
@@ -23,4 +28,3 @@ vim.api.nvim_create_autocmd("BufEnter", {
 		end
 	end
 })
-
