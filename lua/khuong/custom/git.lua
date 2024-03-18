eab3b4e56010ea1cbe17c4dfd65cd02d5d1ee064
khuong <dinhquangkhuong5566@gmail.com>
2024-03-18 09:24:01 +0700

    save git

--- lua/khuong/custom/git.lua
+++ lua/khuong/custom/git.lua
@@ -5,10 +5,14 @@ vim.api.nvim_create_autocmd("BufEnter", {
 				local branch = vim.fn.systemlist("git branch --show-current")[1];
 				vim.cmd("G push origin " .. branch);
 			end, { desc = "push to origin" })
-		else
-			vim.keymap.del("n", "<leader>gp");
 		end
 	end
 })
 
-
+vim.api.nvim_create_autocmd("BufLeave", {
+	callback = function ()
+		if vim.bo.filetype == "fugitive" then
+			vim.keymap.del("n", "<leader>gp");
+		end
+	end
+})
