13c4f1b1bc526126fc70afe1891c0d8e3d52819d
dinhquangkhuong <dinhquangkhuong5566@gmail.com>
2026-04-02 22:31:56 +0700

    sudo write

--- lua/plugins/helper.lua
+++ lua/plugins/helper.lua
@@ -83,7 +83,8 @@ return {
     'lambdalisue/suda.vim',
     lazy = false,
     config = function ()
-      vim.api.nvim_create_user_command("W", "SudaWrite", {})
+      vim.api.nvim_create_user_command("Write", "SudaWrite", {})
+      vim.api.nvim_create_user_command("Read", "SudaRead", {})
     end
   },
   {
